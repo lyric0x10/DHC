0fc996a409805c0a7fe4c69f01218ee13e62b004
lyric0x10 <166757722+lyric0x10@users.noreply.github.com>
2025-01-10 19:34:45 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ if Dropping == true then
         Alt7 = 7826163634,
         Alt8 = 7820540058
     }
-    if game:GetService("Players").LocalPlayer.Name == getgenv().Host then
+    if game:GetService("Players").LocalPlayer.Name == "iahkjadshfkjlash" then
         loadstring(game:HttpGet('https://raw.githubusercontent.com/Crostide/cdhc/main/gui', true))()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/W9K/Da-Hood-Scripts/main/Cash_Counter.lua', true))()
     else
