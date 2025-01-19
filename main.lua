be55c46f4db5f29c3cbaea40f8a34a16ffe9f75a
lyric0x10 <166757722+lyric0x10@users.noreply.github.com>
2025-01-19 15:17:18 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,23 +1,73 @@
 local Dropping = true
 
 if Dropping == true then
-    getgenv().Host = "Lyrics_DHC423"
-    getgenv().FPS = 5
-    getgenv().Alts = {
-        Alt1 = 7825747703,
-        Alt2 = 7817208777,
-        Alt3 = 7817185226,
-        Alt4 = 7817236038,
-        Alt5 = 7817331983,
-        Alt6 = 7817364954,
-        Alt7 = 7826163634,
-        Alt8 = 7820540058
-    }
     if game:GetService("Players").LocalPlayer.Name == "Lyrics_DHC423" then
-        getgenv().prefix = '.'
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/el0001/DHMarket/main/cutescript'))()
+        script_key = "wvnivHKXYeFGfsVvLzpcfgzXFufrJfLf"
+
+        -----------------------
+
+        Account_Cookie = "" -- [OPTIONAL] The accounts cookie, used for account utilities, allows you to reset private server link and accept friend requests directly from the game
+        -- Your account cookie is not shared with anyone or forwarded to a server other than the Roblox one.
+
+        getgenv().FostersSettings = {
+            ["SmallUI"] = true, -- Decrease the size of the UI
+            ["Alts"] = {
+                -- Put your alt ids here, you do NOT need to execute the script on the alts.
+                7825747703,
+                7817208777,
+                7817185226,
+                7817236038,
+                7817331983,
+                7817364954,
+                7826163634,
+                7820540058
+            },
+            ["Logs Webhook"] = "", -- The webhook where logs will be sent, leave at "" to disable
+            ["Proofs Webhook"] = "https://discord.com/api/webhooks/1330630039686877286/CVyLMk5XUgjuWF6MdihOaSU8Dq4v-KQQiB2E5Ucd66YFpp98tLRjeApY3FMeq3yUPN3Z", -- The webhook where proofs will be sent, leave at "" to disable
+            -- Keep in mind every message is filtered by Roblox, {buyer} will turn into the buyers displayname.
+            -- Leaving these messages on default is highly recommended.
+
+            ["Messages"] = {
+                ["Buyer Goal Finished"] = "{buyer} Thank you, leave the game and make sure to vouch!",
+                ["Extra Cash Pickup"] = "{buyer} Stop picking up cash or you will be kicked!",
+                ["Auto Finished Kick Warning"] = "{buyer} Please leave the game or you will be kicked!",
+                ["No Goal Set Pickup Warning"] = "{buyer} Please wait a moment before picking up cash!",
+                ["Shout on set goal"] = "{buyer} Start picking up your cash!",
+                ["Inactivity Warning"] = "{buyer} Please pick up your cash or you will be kicked!",
+                ["Drop/Spend Cash Warning"] = "{buyer} Do not spend or drop cash or you will be kicked!"
+            },
+            ["DHR Utilities v1.51"] = ".gg/dhrutils"
+        }
+
+        -----------------------
+
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cef2dea14de16d7a12c607d7825d3c88.lua"))()
     else
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/W9K/Da-Hood-Scripts/main/FreeVers.lua', true))()
+        script_key = "NNCTXvDvqFijTCCzehmGiYKKEMBlyLFr"
+
+        getgenv().AltControlSettings = {
+            ["Host"] = {
+                ["ID"] = 7874345806,
+                ["Prefix"] = "/"
+            },
+            ["Alts"] = {
+                ["FPS"] = 3,
+                ["RenderingEnabled"] = true,
+                ["Stackable CDrop"] = true,
+                ["IDs"] = {
+                    7825747703,
+                    7817208777,
+                    7817185226,
+                    7817236038,
+                    7817331983,
+                    7817364954,
+                    7826163634,
+                    7820540058
+                }
+            }
+        }
+
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e26feccea2930cf4c02bfd5f151b2361.lua"))()
     end
 else
     script_key = "jyGvAPALEgSsQsVIXAtcfCqrEkpVGupL"
@@ -33,7 +83,16 @@ else
             ["Cycle"] = 1, -- 1 = After dying once.
             ["Time"] = 0, -- 1 = After 1 Minute.
             ["Kick"] = true, -- true = After getting kicked.
-            ["Blacklisted_IDs"] = {7825747703, 7817208777, 7817185226, 7817236038, 7817331983,7817364954, 7826163634, 7820540058} -- If UserID was found ingame, Detects new joining players too.
+            ["Blacklisted_IDs"] = {
+                7825747703,
+                7817208777,
+                7817185226,
+                7817236038,
+                7817331983,
+                7817364954,
+                7826163634,
+                7820540058
+            } -- If UserID was found ingame, Detects new joining players too.
         },
         " discord.gg/iku "
     }
